a04beab8c18f856e20a06a27ff05b9d2c0d78c8a
chathuka18 <147928535+chathuka18@users.noreply.github.com>
2024-02-24 16:27:15 +0530

    Add emailjs/browser dependency and implement email sending functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,6 +16,8 @@ const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
 
+  const linkedInProfileUrl = "https:/www.linkedin.com/in/chathukagamage";
+  const githubProfileUrl = "https://github.com/chathuka18";
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
@@ -101,7 +103,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href={linkedInProfileUrl} target="_blank" rel="noopener noreferrer"
             >
               Linkedin <FaLinkedin size={30} />
             </a>
@@ -109,7 +111,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href={githubProfileUrl} target="_blank" rel="noopener noreferrer"
             >
               Github <FaGithub size={30} />
             </a>
